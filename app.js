f45b5b4d2e76a8e62f6c60e82e421b41379bcd87
Lukas-i-bastun <lukash@live.se>
2022-02-22 14:56:01 +0100

    merge

---
+++ app.js
@@ -0,0 +1,19 @@
+const myFunction = () => {
+    if (true) {
+        console.log("true")
+    } else {
+        console.log("Not true")
+    }
+}
+
+for (let i = 0; i <= 10; i++) {
+console.log(i)
+}
+
+const myObject = {
+    name = "Lukas",
+    age = 23,
+    "Last name": "Hagberg"
+}
+
+console.log(myObject["Last name"])
